e33e069af7b2648498d254c3a80bca8d6e9d394a
Alex Allen <325@holbertonschool.com>
2018-09-20 21:33:17 +0000

    validating puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,8 @@
 # expand nginx user limit
 exec { 'expand nginx user limmit':
   command => '/bin/sed -i 's/15/5000/' /etc/default/nginx',
+}
+
+exec { 'restart webserver':
   command => '/usr/bin/service nginx restart',
 }
