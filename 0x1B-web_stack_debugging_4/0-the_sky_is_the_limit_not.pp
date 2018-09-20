6547d2aa72cecea949119cb3a665e942cf44650d
Alex Allen <325@holbertonschool.com>
2018-09-20 21:39:54 +0000

    escape chars in sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # expand nginx user limit
-exec { 'expand nginx user limmit':
-  command => '/bin/sed -i 's/15/5000/' /etc/default/nginx',
+exec { 'expand nginx user limit':
+  command => '/bin/sed -i \'s/15/5000/\' /etc/default/nginx',
 }
 
 exec { 'restart webserver':
