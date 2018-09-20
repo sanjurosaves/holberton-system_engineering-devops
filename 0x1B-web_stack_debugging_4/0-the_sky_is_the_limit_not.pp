d70ede930a5b3cb40d359d3812b16a317476245a
Alex Allen <325@holbertonschool.com>
2018-09-20 23:04:34 +0000

    repeating sed, rm service path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # expand nginx user limit
 exec { 'expand nginx user limit':
-  command => '/bin/sed -i \'s/15/5000/\' /etc/default/nginx',
+  command => '/bin/sed -i "s/15/4000/g" /etc/default/nginx',
 }
 
 exec { 'restart webserver':
-  command => '/usr/bin/service nginx restart',
+  command => 'service nginx restart',
 }
